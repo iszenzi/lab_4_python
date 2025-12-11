8def8fd82e7e30ce80cd605c478fefd8b1f2654a
ISZenzi <zenzirovi@gmail.com>
2025-12-12 00:03:51 +0300

    add classes FictionBook and NonFictionBook

--- src/book.py
+++ src/book.py
@@ -8,3 +8,19 @@ class Book:
 
     def __repr__(self) -> str:
         return f'Book(title="{self.title}", author="{self.author}", year={self.year}, genre="{self.genre}", isbn="{self.isbn}")'
+
+
+class FictionBook(Book):
+    def __init__(self, title: str, author: str, year: int, genre: str, isbn: str):
+        super().__init__(title, author, year, genre, isbn)
+
+    def __repr__(self) -> str:
+        return f'FictionBook(title="{self.title}", author="{self.author}", year={self.year}, genre="{self.genre}", isbn="{self.isbn}")'
+
+
+class NonFictionBook(Book):
+    def __init__(self, title: str, author: str, year: int, genre: str, isbn: str):
+        super().__init__(title, author, year, genre, isbn)
+
+    def __repr__(self) -> str:
+        return f'NonFictionBook(title="{self.title}", author="{self.author}", year={self.year}, genre="{self.genre}", isbn="{self.isbn}")'
